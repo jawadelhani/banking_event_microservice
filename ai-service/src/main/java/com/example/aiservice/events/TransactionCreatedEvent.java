192d450c00e64ee6046711dd1f96b48149ffe853
Jawad <jawadfoot.2019@gmail.com>
2026-07-14 12:03:34 +0100

    Implement events

---
+++ ai-service/src/main/java/com/example/aiservice/events/TransactionCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.example.aiservice.events;
+
+import com.example.aiservice.entities.TransactionType;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionCreatedEvent {
+    private UUID transactionId;
+    private UUID accountId;
+    private UUID clientId;
+    private String accountNumber;
+    private BigDecimal amount;
+    private TransactionType type;
+    private LocalDateTime createdAt;
+}
