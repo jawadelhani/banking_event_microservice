a523c4a96e0924cc6dd22236cd181047241c71d1
Jawad <jawadfoot.2019@gmail.com>
2026-07-14 12:01:59 +0100

    set up ai service

---
+++ ai-service/src/main/java/com/example/aiservice/AiServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.aiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServiceApplication.class, args);
+    }
+
+}
