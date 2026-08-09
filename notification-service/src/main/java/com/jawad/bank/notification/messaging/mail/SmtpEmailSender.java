5c5aa6820e7044e5f3566fabbc26f3b977bbcea7
Jawad <jawadfoot.2019@gmail.com>
2026-08-09 14:58:41 +0100

    Change SimpleMailMessage to MimeMessage

--- notification-service/src/main/java/com/jawad/bank/notification/messaging/mail/SmtpEmailSender.java
+++ notification-service/src/main/java/com/jawad/bank/notification/messaging/mail/SmtpEmailSender.java
@@ -1,9 +1,10 @@
 package com.jawad.bank.notification.messaging.mail;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,11 +16,16 @@ public class SmtpEmailSender implements EmailSender {
 
     @Override
     public void send(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
-        log.info("Email sent to {}", to);
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true); // true indicates HTML
+            mailSender.send(message);
+            log.info("Email sent to {}", to);
+        } catch (Exception e) {
+            log.error("Failed to send HTML email to {}", to, e);
+        }
     }
 }
